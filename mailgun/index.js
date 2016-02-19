870fcd4d0c8fc32886821d1e5c2c3e58c763a702
unknown <IT@TRIVENDER16818-IT.intexindia.com>
2016-02-19 12:49:15 +0530

    minor

--- mailgun/index.js
+++ mailgun/index.js
@@ -1,12 +1,11 @@
-module.exports=function(mailgun){
+module.exports=function(mailgun,req){
+
   var apiKey='key-42e20f5a90e601ebbdb9a52d8164733a';
   var domainName='sandbox2dbe16208a5c4ca993c7d563adcac177.mailgun.org';
   var sender='pratap.jatintripathi@gmail.com';
 
-  mailgun.messages().send(data,function(err,body){
-    if(err) throw err;
-    
-  });
+  var mailgun=new mailgun({apikey:apikey,domain:domainName});
 
-  verification(mailgun,apikey,domainName,sender);
+  verification(req);
+  return data;
 };

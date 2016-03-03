ebeacd668ae4fcd2d7c33f324dfda7cac6fb496d
Jatin Tripathi <pratap.jatintripathi@gmail.com>
2016-03-03 17:25:54 +0530

    Delete verification.js

--- mailgun/verification.js
+++
@@ -1,9 +0,0 @@
-module.exports=function(req){
-  var data={
-    from:'Jatin Tripathi',
-    to:req.param.Email,
-    subject:'My Verification Mail',
-    html:'Hey, you trying my application hah. Alright then <a href="http://0.0.0.0:3030/home?' + req.params.mail + '">Click Here to authenticate your account</a>'
-  };
-  return data;
-};
